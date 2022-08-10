3af44e94e45681fe427bf054b4098f4babdc12c7
smartbrein <52868184+sbpythonista@users.noreply.github.com>
2022-08-10 05:20:20 +0100

    path

--- img_api/images/url.py
+++ img_api/images/url.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from django.views.generic import TemplateView
+from .views import ImagesListView
 
 
 app_name = 'images'
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name="images/index.html")),
+    path('images/', ImagesListView.as_view(), name='images'),
 ]
