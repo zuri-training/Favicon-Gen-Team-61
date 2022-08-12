2fd8137453442cf1fe3176f05f4c02b7eae6bf33
Theonrex <theonrex1@gmail.com>
2022-08-12 22:02:02 +0100

    dark_mode & documentation fix

--- Backend/src/generator/static/js/main.js
+++ Backend/src/generator/static/js/main.js
@@ -1,12 +1,42 @@
 //Dark & Light Mode
-document.getElementById("darkmode").onclick = function() {darkMode()};
+// document.getElementById("darkmode").onclick = function() {darkMode()};
 
-function darkMode() {
-    var dark = document.body;
+// function darkMode() {
+//     var dark = document.body;
 
-    dark.classList.toggle("dark-mode");
+//     dark.classList.toggle("dark-mode");
+// };
+
+
+
+const toggleBtn = document.getElementById("darkmode");
+const theme = document.body;
+let darkMode = localStorage.getItem("dark-mode");
+
+const enableDarkMode = () => {
+  theme.classList.add("dark-mode");
+  toggleBtn.classList.add("slider:before");
+  localStorage.setItem("dark-mode", "enabled");
 };
 
+const disableDarkMode = () => {
+  theme.classList.remove("dark-mode");
+  toggleBtn.classList.remove("slider:before");
+  localStorage.setItem("dark-mode", "disabled");
+};
+
+if (darkMode === "enabled") {
+  enableDarkMode(); // set state of darkMode on page load
+}
+
+toggleBtn.addEventListener("click", (e) => {
+  darkMode = localStorage.getItem("dark-mode"); // update darkMode when clicked
+  if (darkMode === "disabled") {
+    enableDarkMode();
+  } else {
+    disableDarkMode();
+  }
+});
 
 // // document.getElementById("darkmode").onclick = function() {darkMode()};
 
