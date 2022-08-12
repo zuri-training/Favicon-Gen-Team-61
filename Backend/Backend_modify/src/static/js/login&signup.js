96c6db5fbc655a83d62936eac06f8a3e684aa1f1
Theonrex <theonrex1@gmail.com>
2022-08-12 06:44:58 +0100

    template fix

--- Backend/Backend_modify/src/static/js/login&signup.js
+++
@@ -1,7 +0,0 @@
-{% load static %}
-//Dark & Light Mode
-document.getElementById("darkmode").onclick = function() {darkMode()};
-
-function darkMode() 
-    var dark = document.body;
-    dark.classList.toggle("dark-mode");
