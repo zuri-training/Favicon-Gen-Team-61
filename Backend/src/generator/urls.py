91d468ce50ea7c77abc1a7d08405263129297932
George-smart <georgechinedu2018@gmail.com>
2022-08-13 21:25:03 +0100

    Update the reset password urls

--- Backend/src/generator/urls.py
+++ Backend/src/generator/urls.py
@@ -36,15 +36,15 @@ urlpatterns = [
      name="reset_password"),
 
     path('reset_password_sent/', 
-        auth_views.PasswordResetDoneView.as_view(), 
+        auth_views.PasswordResetDoneView.as_view(template_name='password_reset_sent.html'), 
         name="password_reset_done"),
 
     path('reset/<uidb64>/<token>/',
-     auth_views.PasswordResetConfirmView.as_view(), 
+     auth_views.PasswordResetConfirmView.as_view(template_name='password_reset_form.html'), 
      name="password_reset_confirm"),
 
     path('reset_password_complete/', 
-        auth_views.PasswordResetCompleteView.as_view(), 
+        auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_done.html'), 
         name="password_reset_complete"),
 
 
